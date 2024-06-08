2882629f80092d56dbca0110151debd046e797e0
xChocoCRISpis <crisneeda@gmail.com>
2024-06-08 16:45:36 -0600

    login conectado a mongo

--- API_MySql/src/resources/usuario/usuario.controllers.js
+++ API_MySql/src/resources/usuario/usuario.controllers.js
@@ -1,4 +1,5 @@
 import { pool } from "../../db/connect.js";
+import axios from 'axios';
 import { createCustomError } from "../../errors/customErrors.js";
 import { tryCatchWrapper } from "../../middlewares/tryCatchWrapper.js";
 
@@ -18,20 +19,21 @@ export const login = tryCatchWrapper(async (req, res, next) => {
             return next(createCustomError("Invalid credentials", 401));
         }
 
+        // Enviar la respuesta inicial
         res.status(200).json({
             idUsuario: result[0].idUsuario,
             tipo: result[0].tipo
         });
+
+        // Realizar la solicitud a la API de MongoDB después de enviar la respuesta inicial
         try {
-            const response = await axios.get('http://localhost:9000/api/crear?id_usuario=' + idUsuario + '&fecha=08/06/2024&hora=4:14&accion=login');
-            const mongoItems = response.data;
-            res.json(mongoItems);
+            const response = await axios.post('http://localhost:9000/crear?id_usuario=' + result[0].idUsuario + '&fecha=08/06/2024&hora=4:14&accion=login');
+            // No es necesario enviar otra respuesta, pero puedes registrar la respuesta de MongoDB si lo deseas
+            console.log(response.data);
         } catch (error) {
-            res.status(500).send('Error al consumir la API de MongoDB');
+            console.error('Error al consumir la API de MongoDB', error);
         }
-    }
-    
-    catch (error) {
-        next(createCustomError(`Database query failed: ${error.message}`, 500));
+    } catch (error) {
+        next(createCustomError("Database query failed", 500));
     }
 });
